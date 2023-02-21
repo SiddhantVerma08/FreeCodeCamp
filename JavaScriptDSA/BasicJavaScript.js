54e81207453d28be19953c6154576893ff64770e
Siddhant <jointosid@gmail.com>
2023-02-21 16:46:47 +0530

    Solved Basic JS 56 to 102

--- JavaScriptDSA/BasicJavaScript.js
+++ JavaScriptDSA/BasicJavaScript.js
@@ -400,3 +400,742 @@ function processArg(num) {
 
 processed = processArg(7)
 
+// Stand in Line
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    const removed = arr.shift();
+    return removed;
+    // Only change code above this line
+  }
+  
+  // Setup
+  let testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
+
+//   Understanding Boolean Values
+function welcomeToBooleans() {
+    // Only change code below this line
+    
+    return true; // Change this line
+  
+    // Only change code above this line
+  }
+
+//   Use Conditional Logic with If Statements
+function trueOrFalse(wasThatTrue) {
+    // Only change code below this line
+      if (wasThatTrue==true){
+        return "Yes, that was true";
+      }
+      else{
+        return "No, that was false"
+      }
+  
+  
+    // Only change code above this line
+  
+  }
+
+//   Comparison with the Equality Operator
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
+
+// Comparison with the Strict Equality Operator
+// Setup
+function testStrict(val) {
+    if (val === 7) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testStrict(10);
+
+//   Practice comparing different values
+// Setup
+function compareEquality(a, b) {
+    if (a == b) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
+
+//   Comparison with the Inequality Operator
+// Setup
+function testNotEqual(val) {
+    if (val !=99) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testNotEqual(10);
+
+//   Comparison with the Strict Inequality Operator
+// Setup
+function testStrictNotEqual(val) {
+    if (val !== 17) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testStrictNotEqual(10);
+
+//   Comparison with the Greater Than Operator
+function testGreaterThan(val) {
+    if (val > 100) {  // Change this line
+      return "Over 100";
+    }
+  
+    if (val > 10) {  // Change this line
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+  }
+  
+  testGreaterThan(10);
+
+// Comparison with the Greater Than Or Equal To Operator
+function testGreaterOrEqual(val) {
+    if (val >= 20) {  // Change this line
+      return "20 or Over";
+    }
+  
+    if (val >= 10) {  // Change this line
+      return "10 or Over";
+    }
+  
+    return "Less than 10";
+  }
+  
+  testGreaterOrEqual(10);
+
+//   Comparison with the Less Than Operator
+function testLessThan(val) {
+    if (val < 25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val < 55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  testLessThan(10);
+
+//   Comparison with the Less Than Or Equal To Operator
+function testLessOrEqual(val) {
+    if (val <=12) {  // Change this line
+      return "Smaller Than or Equal to 12";
+    }
+  
+    if (val <= 24) {  // Change this line
+      return "Smaller Than or Equal to 24";
+    }
+  
+    return "More Than 24";
+  }
+  
+  testLessOrEqual(10);
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >=25) {
+    return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+testLogicalAnd(10);
+
+// Comparisons with the Logical And Operator
+function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val <= 50 && val >=25) {
+      return "Yes";
+    }
+  
+    // Only change code above this line
+    return "No";
+  }
+  
+  testLogicalAnd(10);
+
+//   Comparisons with the Logical Or Operator
+function testLogicalOr(val) {
+    // Only change code below this line
+  
+    if (val < 10 || val > 20) {
+      return "Outside";
+    }
+  
+  
+    // Only change code above this line
+    return "Inside";
+  }
+  
+  testLogicalOr(15);
+
+//   Introducing Else Statements
+function testElse(val) {
+    let result = "";
+    // Only change code below this line
+  
+    if (val > 5) {
+      result = "Bigger than 5";
+    }
+  
+    else {
+      result = "5 or Smaller";
+    }
+  
+    // Only change code above this line
+    return result;
+  }
+  
+  testElse(4);
+
+//   Introducing Else If Statements
+function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    }
+  
+    else if (val < 5) {
+      return "Smaller than 5";
+    }
+    else{
+    return "Between 5 and 10";
+  }
+  }
+  
+  testElseIf(7);
+
+//   Logical Order in If Else Statements
+function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    
+    } else if (val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+    
+  }
+  
+  orderMyLogic(7);
+
+//   Chaining If Else Statements
+function testSize(num) {
+    // Only change code below this line
+    if (num < 5){
+        return "Tiny"
+      }
+      else if (num < 10){
+        return "Small"
+      }
+      else if (num < 15){
+        return "Medium"
+      }
+      else if (num < 20){
+        return "Large"
+      }
+      else{
+        return "Huge"
+      }
+    // Only change code above this line
+  }
+  
+  testSize(7);
+
+//   Golf Code
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+   return strokes === 1
+    ? names[0]
+    : strokes <= par - 2
+    ? names[1]
+    : strokes === par - 1
+    ? names[2]
+    : strokes === par
+    ? names[3]
+    : strokes === par + 1
+    ? names[4]
+    : strokes === par + 2
+    ? names[5]
+    : names[6];
+  // Only change code above this line
+}
+
+golfScore(5, 4);
+
+// Selecting from Many Options with Switch Statements
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val){
+      case 1:
+        answer = 'alpha';
+        break;
+      case 2:
+        answer = 'beta';
+        break;
+      case 3:
+        answer = 'gamma';
+        break;
+      case 4:
+        answer = 'delta';
+        break;
+    }
+
+    // Only change code above this line
+    return answer;
+  }
+  caseInSwitch(1);
+
+//   Adding a Default Option in Switch Statements
+function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val){
+      case "a":
+        answer = "apple"
+        break;
+      case "b":
+        answer = "bird"
+        break;
+      case "c":
+        answer = "cat"
+        break;
+      default:
+        answer = "stuff"
+        break;
+    }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  switchOfStuff(1);
+
+//   Multiple Identical Options in Switch Statements
+function sequentialSizes(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val){
+      case 1:
+      case 2:
+      case 3:
+      answer = "Low";
+      break;
+      case 4:
+      case 5:
+      case 6:
+      answer = "Mid";
+      break;
+      case 7:
+      case 8:
+      case 9:
+      answer = "High";
+      break;
+    }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  sequentialSizes(1);
+
+//   Replacing If Else Chains with Switch
+function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val){
+      case "bob":
+      answer = "Marley";
+      break;
+      case 42:
+      answer = "The Answer";
+      break;
+      case 1:
+      answer = "There is no #1";
+      break;
+      case 99:
+      answer = "Missed me by this much!";
+      break;
+      case 7:
+      answer = "Ate Nine";
+      break;
+    }
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  chainToSwitch(7);
+
+//   Returning Boolean Values from Functions
+function isLess(a, b) {
+    // Only change code below this line
+    return a<b;
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
+
+//   Return Early Pattern for Functions
+// Setup
+function abTest(a, b) {
+    // Only change code below this line
+      if(a === 0 || b === 0){
+        return 0
+      }
+      else if(a < 0 || b < 0){
+        return undefined
+      }
+    // Only change code above this line
+  
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  
+  abTest(2,2);
+
+//   Counting Cards
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  if (card <= 6) 
+    count++;
+  else if (card >="10")
+    count--;
+  return count + (count > 0 ? ' Bet' : ' Hold');
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+// Build JavaScript Objects
+const myDog = {
+    // Only change code below this line
+    name : "Rishabh",
+    legs : 4,
+    tails : 1,
+    friends : ["Santnu", "Tanay"]
+    // Only change code above this line
+  };
+
+//   Accessing Object Properties with Dot Notation
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  
+  // Only change code below this line
+  const hatValue = testObj.hat;      // Change this line
+  const shirtValue = testObj.shirt;    // Change this line
+
+//   Accessing Object Properties with Bracket Notation
+// Setup
+const testObjs = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  
+  // Only change code below this line
+  const entreeValue = testObj["an entree"];   // Change this line
+  const drinkValue = testObj["the drink"];    // Change this line
+
+//   Accessing Object Properties with Variables
+// Setup
+const testObjss = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Only change code below this line
+  const playerNumber = 16;  // Change this line
+  const player = testObj[playerNumber];   // Change this line
+
+//   Updating Object Properties
+// Setup
+const myDogs = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+
+  
+  // Only change code below this line
+  
+  myDog["name"] = "Happy Coder"
+
+//   Add New Properties to a JavaScript Object
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  
+  myDog.bark = "woof"
+
+//   Delete Properties from a JavaScript Object
+// Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  
+  // Only change code below this line
+  delete myDog.tails;
+
+//   Using Objects for Lookups
+// Setup
+function phoneticLookup(val) {
+    let result = "";
+  
+    // Only change code below this line
+    const data = {
+      "alpha": "Adams",
+      "bravo": "Boston",
+      "charlie": "Chicago",
+      "delta": "Denver",
+      "echo": "Easy",
+      "foxtrot": "Frank"
+    }
+    const value = val
+    result = data[value]
+    // Only change code above this line
+    return result;
+  }
+  
+  phoneticLookup("charlie");
+
+//   Testing Objects for Properties
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+    if (obj.hasOwnProperty(checkProp)){
+      return obj[checkProp]
+    } else{
+      return "Not Found"
+    }
+    // Only change code above this line
+  }
+
+//   Manipulating Complex Objects
+  const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+        },
+      {
+      "artist": "Daft Punk",
+      "title": "Homework",
+      "release_year": 1997,
+      "formats": [ 
+        "CD", 
+        "Cassette", 
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+  
+//   Accessing Nested Objects
+const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  const gloveBoxContents = myStorage.car.inside["glove box"];
+
+//   Accessing Nested Arrays
+const myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+  ];
+  
+  const secondTree = myPlants[1].list[2];
+
+//   Record Collection
+// Setup
+const recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+  
+  // Only change code below this line
+  function updateRecords(records, id, prop, value) {
+    if (prop !== "tracks" && value !==""){
+      records[id][prop] = value;
+    }
+    else if (prop === "tracks" && value !=="" && records[id].hasOwnProperty("tracks") === false){
+      records[id][prop] = [value];
+    }
+    else if (prop === "tracks" && value !=="" ){
+      records[id][prop].push(value)
+    }
+    else if (value ===""){
+      delete records[id][prop];
+    }
+    return records;
+  }
+  
+  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+//   Iterate with JavaScript While Loops
+// Setup
+const myArray = [];
+
+// Only change code below this line
+let i = 5;
+
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
+
+// Iterate with JavaScript For Loops
+// Setup
+const myArray = [];
+
+for (let i = 1; i<6; i++){
+  myArray.push(i)
+}
+// Only change code below this line
+
+// Iterate Odd Numbers With a For Loop
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i<10; i+=2){
+  myArray.push(i);
+}
+
+// Count Backwards With a For Loop
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 9; i > 0; i-= 2){
+  ourArray.pus(i)
+}
+
+// Iterate Through an Array with a For Loop
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+let total = 0
+for (let i = 0; i < myArr.length; i++){
+  total += myArr[i];
+}
+
+// Nesting For Loops
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for (let i = 0; i < arr.length; i++){
+      for (let j = 0; j < arr[i].length; j++){
+        product *= arr[i][j]
+      }
+    }
+    // Only change code above this line
+    return product;
+  }
+  
+  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+// Iterate with JavaScript Do...While Loops
+// Setup
+const myArray = [];
+let k = 10;
+
+// Only change code below this line
+
+do {
+  myArray.push(i);
+  i++;
+} while (k < 10)
+
+// Replace Loops using Recursion
