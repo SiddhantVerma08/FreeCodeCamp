20908de4646dec9a243092a925683263dfc8c888
Siddhant <jointosid@gmail.com>
2023-02-21 20:33:58 +0530

    Solved Basic JS 103 to 113

--- JavaScriptDSA/BasicJavaScript.js
+++ JavaScriptDSA/BasicJavaScript.js
@@ -1139,3 +1139,139 @@ do {
 } while (k < 10)
 
 // Replace Loops using Recursion
+function sum(arr, n) {
+    // Only change code below this line
+    if (n <= 0){
+      return 0;
+    } else{
+      return sum(arr, n - 1) + arr[n - 1];
+    }
+    // Only change code above this line
+  }
+
+//   Profile Lookup
+// Setup
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+  
+  function lookUpProfile(name, prop) {
+    // Only change code below this line
+    for (let i = 0; i < contacts.length; i++) {
+      if (name === contacts[i].firstName) {
+        if (contacts[i].hasOwnProperty(prop)) {
+          return contacts[i][prop];
+        }
+        else {
+          return "No such property";
+        }
+      }
+    }
+    return "No such contact";
+    // Only change code above this line
+  }
+  
+  lookUpProfile("Akira", "likes");
+
+//   Generate Random Fractions with JavaScript
+function randomFraction() {
+
+    // Only change code below this line
+    let number = 0;
+  
+    while(number === 0){
+      number = Math.random();
+    }
+    return number
+    // Only change code above this line
+  }
+
+//   Generate Random Whole Numbers with JavaScript
+function randomWholeNum() {
+
+    // Only change code below this line
+    return Math.floor(Math.random() * 10);
+  }
+
+//   Generate Random Whole Numbers within a Range
+function randomRange(myMin, myMax) {
+    // Only change code below this line
+    
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+    // Only change code above this line
+  }
+
+//   Use the parseInt Function
+function convertToInteger(str) {
+    return parseInt(str)
+  }
+  
+  convertToInteger("56");
+
+//   Use the parseInt Function with a Radix
+function convertToInteger(str) {
+    return parseInt(str, 2);
+  }
+  
+  convertToInteger("10011");
+
+//   Use the Conditional (Ternary) Operator
+function checkEqual(a, b) {
+    return a == b ? "Equal" : "Not Equal"
+  }
+  
+  checkEqual(1, 2);
+
+//   Use Multiple Conditional (Ternary) Operators
+function checkSign(num) {
+    return (num > 0) ? 'positive' : (num < 0) ? 'negative': 'zero';
+  }
+  
+  checkSign(10);
+
+//   Use Recursion to Create a Countdown
+// Only change code below this line
+function countdown(n){
+    if (n < 1) {
+      return [];
+    } else {
+      const countArray = countdown(n - 1);
+      countArray.unshift(n);
+      return countArray;
+    }
+  }
+  // Only change code above this line
+
+//   Use Recursion to Create a Range of Numbers
+function rangeOfNumbers(startNum, endNum) {
+    if (endNum < startNum) {
+      return [];
+    } else {
+      const numbers = rangeOfNumbers(startNum, endNum - 1);
+      numbers.push(endNum);
+      return numbers;
+    }
+  };
