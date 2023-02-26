d3e33fec18f7e602aff7033c48925c0007fc1a5f
Siddhant <jointosid@gmail.com>
2023-02-26 20:04:28 +0530

    Basic data scripting

--- JavaScriptDSA/BasicDataScripting.js
+++ JavaScriptDSA/BasicDataScripting.js
@@ -80,3 +80,76 @@ function repeatStringNumTimes(str, num) {
   repeatStringNumTimes("abc", 3);
 
 // Truncate a String
+function truncateString(str, num) {
+    return str.length > num ? str.slice(0, num)+"...":str}
+  
+  truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+// Finders Keepers
+function findElement(arr, func) {
+    return arr.find(func)
+  }
+  
+  findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+// Boo Who
+function booWho(bool) {
+    return typeof bool === "boolean";
+  }
+  
+  booWho(null);
+// Title Case a Sentence
+function titleCase(str) {
+    return str
+    .toLowerCase()
+    .split(" ")
+    .map(val => val.replace(val.charAt(0), val.charAt(0).toUpperCase()))
+    .join(" ");
+    ;
+  }
+  
+  titleCase("I'm a little tea pot");
+
+// Slice and Splice
+function frankenSplice(arr1, arr2, n) {
+    return [...arr2.slice(0, n), ...arr1, ...arr2.slice(n)];
+  }
+  
+  frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+// Falsy Bouncer
+function bouncer(arr) {
+    return arr.filter(Boolean);
+  }
+  
+  bouncer([7, "ate", "", false, 9]);
+
+// Where do I Belong
+function getIndexToIns(arr, num) {
+  return arr.filter(val => num >val).length;
+}
+
+getIndexToIns([40, 60], 50);
+
+// Mutations
+function mutation(arr) {
+  return arr[1]
+  .toLowerCase()
+  .split("")
+  .every(function(letter) {
+  return arr[0].toLowerCase().indexOf(letter) !== -1;
+    });
+}
+
+mutation(["hello", "hey"]);
+
+// Chunky Monkey
+function chunkArrayInGroups(arr, size) {
+  const newArr = [];
+  while (arr.length > 0) {
+    newArr.push(arr.splice(0, size));
+  }
+  return newArr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
