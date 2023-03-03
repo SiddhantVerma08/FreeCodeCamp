a155a159db5645e3dfe830acf84b37b48d2ff82b
Siddhant <jointosid@gmail.com>
2023-03-03 19:43:14 +0530

    9 and 10

---
+++ JavaScriptDSA/IntermediateAlgorithmScripting/SortedUnion.js
@@ -0,0 +1,8 @@
+function uniteUnique() {
+    return [...arguments]
+        .flat()
+        .filter((item, ind, arr) => arr.indexOf(item) === ind);
+    ;
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
