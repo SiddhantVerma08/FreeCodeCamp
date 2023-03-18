f628b6f12fa04f5308313dce92d378ce256da52c
Siddhant <jointosid@gmail.com>
2023-03-18 23:02:25 +0530

    Solved 20

--- Front End Development Libraries/React/part.js
+++ Front End Development Libraries/React/part.js
@@ -363,3 +363,28 @@ class App extends React.Component {
       );
     }
   };
+
+// Review Using Props with Stateless Functional Components
+class CampSite extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <Camper/>
+        </div>
+      );
+    }
+  };
+  // Change code below this line
+  const Camper = props => <p>{props.name}</p>;
+  
+  Camper.defaultProps = {
+    name: "CamperBot"
+  };
+  
+  Camper.propTypes = {
+    name: PropTypes.string.isRequired
+  };
+  
